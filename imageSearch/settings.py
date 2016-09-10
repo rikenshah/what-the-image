3cb3f9121ad6cf759398a33d812f57c1b5d69b3c
rikenshah <riken.1996@gmail.com>
2016-09-10 17:48:41 +0530

    Auth Form

--- imageSearch/settings.py
+++ imageSearch/settings.py
@@ -58,7 +58,7 @@ os.path.join(BASE_DIR, "static"),
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [templates],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+    
+
+LOGIN_REDIRECT_URL = '/'
--- imageSearch/urls.py
+++ imageSearch/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include,url
 from django.contrib import admin
 
+from django.contrib.auth import views
+from accounts.forms import LoginForm
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('accounts.urls')),
+	url(r'^login/$', views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name='login'),
+	url(r'^logout/$', views.logout, {'next_page': '/login'}), 
 ]
